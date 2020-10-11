4113c572a1fa7e9715b8b028ba624e43f5c3947e
davlgd <david@nextinpact.com>
2020-10-11 19:17:18 +0200

    Ajout de la macro cutomPrint!()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+macro_rules! customPrint {
+    () => {
+        println!("Hello, World!");
+    };
+    ($dest:expr) => {
+        println!("Hello, {}!", $dest);
+    };
+    ($act:expr => $dest:expr) => {
+        println!("{} for {}!", $act, $dest);
+    };
+}
+
 fn main() {
     println!("{}, {crowd} {1}!", "Hello", 2*21, crowd = "World");
 }
